bc95cd1d3c0d7b9c73f30c14904b4a5506b4f184
iwaken71 <>
2022-06-11 13:22:13 +0900

    edit game.ts & index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// import 'babylonjs-loaders';
+import { Game } from './game';
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Set global variable for cannonjs physics engine
+  let game = new Game('renderCanvas');
+  game.createScene();
+  game.animate();
+});
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import './style.css'
+import * as BABYLON from '@babylonjs/core';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const renderCanvas = <HTMLCanvasElement>document.getElementById('renderCanvas');
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const main = (canvas: HTMLCanvasElement) => {
+  const engine = new BABYLON.Engine(canvas, true);
+  const scene = new BABYLON.Scene(engine);
+  const camera = new BABYLON.ArcRotateCamera("camera", 0, 1.3, 10, BABYLON.Vector3.Zero(), scene);
+  const light = new BABYLON.DirectionalLight("light",new BABYLON.Vector3(0, Math.PI/4, Math.PI/4),scene);
+  camera.attachControl(canvas);
+  const cube = BABYLON.MeshBuilder.CreateBox("cube",{size:1});
+  scene.debugLayer.show();
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
+};
+
+main(renderCanvas);
